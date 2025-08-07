d706e165782c4abc8528c51ba7f1142e491d14cc
Veronika Butkevich <vbutkevich@networkoptix.com>
2025-08-07 15:35:20 +0200

    Remove filtering of compile_commands.json

--- tools/sast-pipeline/Dockerfiles/builder/run_inside_builder.py
+++ tools/sast-pipeline/Dockerfiles/builder/run_inside_builder.py
@@ -8,22 +8,22 @@ import os
 load_dotenv(dotenv_path="/app/.env")
 
 if __name__ == "__main__":
-
-    print("[*] Filtering compile_commands.json to first entry only (for test)...")
-
-    cc_path = Path("/workspace/build-tmp/nx_open/build/compile_commands.json")
-    if cc_path.exists():
-        with cc_path.open() as f:
-            data = json.load(f)
-
-        filtered = data[:5]
-
-        with cc_path.open("w") as f:
-            json.dump(filtered, f, indent=2)
-
-        print("[✓] compile_commands.json filtered.")
-    else:
-        print(f"[!] File not found: {cc_path}")
+    #
+    # print("[*] Filtering compile_commands.json to first entry only (for test)...")
+    #
+    # cc_path = Path("/workspace/build-tmp/nx_open/build/compile_commands.json")
+    # if cc_path.exists():
+    #     with cc_path.open() as f:
+    #         data = json.load(f)
+    #
+    #     filtered = data[:5]
+    #
+    #     with cc_path.open("w") as f:
+    #         json.dump(filtered, f, indent=2)
+    #
+    #     print("[✓] compile_commands.json filtered.")
+    # else:
+    #     print(f"[!] File not found: {cc_path}")
 
     print("[>] Running analyzer...")
 

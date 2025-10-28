380d3708f40ccf0fb03e8869740c5488713d53f5
Veronika Butkevich <vbutkevich@networkoptix.com>
2025-10-28 15:43:27 +0100

    Optimize imports

--- tools/utils/push_to_server.sh
+++ tools/utils/push_to_server.sh
@@ -1,7 +1,7 @@
 # Settings
 REMOTE_USER="root"
 REMOTE_HOST="sastserver"
-REMOTE_DIR="/root/work/django-DefectDojo"
+REMOTE_DIR="/root/work/aist-defect-dojo"
 
 # 1. Sync project on remote machine
 rsync -avz --delete --exclude '.git' --exclude '__pycache__' --exclude 'sast-combinator/analyses_result' ../../../ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
